6a8f056ac7f6cc442dfb0926d296f9f3174c285e
Jhon-Develop <asprillajhon73@gmail.com>
2024-08-30 09:52:34 -0500

    start project

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                cWhite: "#FFFCD1",
+                cGreen: "#07D8AE",
+                cPurple: "#8C04BC",
+                cYellow: "#F2E533",
+                cBlack: "#000000",
+            },
+            fontFamily: {
+                MontserratItalic: ['MontserratItalic'],
+                MontserratRegular: ['MontserratRegular'],
+                MontserratMedium: ['MontserratMedium'],
+                MontserratSemibold: ['MontserratSemibold'],
+                MontserratBold: ['MontserratBold'],
+                ComfortaaLight: ['ComfortaaLight'],
+                ComfortaaRegular: ['ComfortaaRegular'],
+                ComfortaaMedium: ['ComfortaaMedium'],
+                ComfortaaSemibold: ['ComfortaaSemibold'],
+                ComfortaaBold: ['ComfortaaBold'],
+            },
+        },
+    },
+    plugins: [],
+}
